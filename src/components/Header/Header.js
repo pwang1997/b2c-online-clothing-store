1fb21d72c92c364a602f5eb7c6d8326af0b49dab
zwang684 <puck19970418@gmail.com>
2022-09-23 10:10:41 -0400

    ADD base components and pages

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+/**
+ * Requirements:
+ *  1. Header manages user state(guess/logged user)
+ *  2. Header contains a search bar to query products.
+ *  3. Header has an OAuth login button.
+ *  4. Header has shopping cart link.
+ *  5. Header has links to various product categories.
+ */
