663d8d44e73911d79004d3327700e82e21cdf73c
zwang684 <puck19970418@gmail.com>
2022-10-02 10:57:46 -0400

    ADD responsive user signup form

--- view/src/components/Header/UserItem/UserLogin.js
+++ view/src/components/Header/UserItem/UserLogin.js
@@ -2,29 +2,21 @@ import {useEffect, useState} from "react";
 
 import IconButton from "@mui/material/IconButton";
 import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
-import {Button, Menu, MenuItem, TextField} from "@mui/material";
-import Grid from '@mui/material/Unstable_Grid2';
+import {
+    Button,
+    Menu,
+    MenuItem
+} from "@mui/material";
 import MoreIcon from '@mui/icons-material/MoreVert';
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 350,
-    minHeight: 500,
-    bgcolor: 'background.paper',
-    p: 4
-};
+// import {UserContext} from "../../../App";
+import UserLoginForm from "./UserLoginForm";
 
 const UserLogin = () => {
     const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024 && window.innerHeight <= 1024);
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    // const firebaseUserCollectionRef = useContext(UserContext);
 
     useEffect(() => {
         // toggle condition for displaying mobile version
@@ -43,52 +35,6 @@ const UserLogin = () => {
         open ? handleClose() : handleOpen();
     };
 
-    const ModalWebLogin = () => {
-        return (
-            <Modal
-                aria-labelledby="web-modal-title"
-                open={open}
-                onClose={handleClose}
-                closeAfterTransition
-            >
-                <Fade in={open}>
-                    <Box sx={style}>
-                        <Grid container spacing={2}>
-                            <Grid item marginTop={6} xs={12} md={12}>
-                                <Typography id="web-modal-title" variant="h6" component="h6" align={"center"}>
-                                    Welcome to Clothing Store
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={12} md={12}>
-                                <TextField id="email" label="Email" fullWidth required/>
-                            </Grid>
-                            <Grid item xs={12} md={12}>
-                                <TextField
-                                    id="outlined-password-input"
-                                    label="Password"
-                                    type="password"
-                                    autoComplete="current-password"
-                                    fullWidth
-                                    required
-                                />
-                            </Grid>
-                            <Grid item xs={12} md={6}>
-                                <Button variant="outlined" fullWidth>Log in</Button>
-                            </Grid>
-                            <Grid item xs={12} md={6}>
-                                <Button variant="outlined" fullWidth>Sign up</Button>
-                            </Grid>
-
-                            <Grid item xs={12}>
-                                <Button variant="outlined" fullWidth>Login with Google</Button>
-                            </Grid>
-                        </Grid>
-                    </Box>
-                </Fade>
-            </Modal>
-        );
-    };
-
     const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
     const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
     const handleMobileMenuClose = () => {
@@ -98,6 +44,7 @@ const UserLogin = () => {
         setMobileMoreAnchorEl(event.currentTarget);
     };
     const mobileMenuId = 'primary-search-account-menu-mobile';
+
     const renderMobileMenu = (
         <Menu
             anchorEl={mobileMoreAnchorEl}
@@ -172,7 +119,6 @@ const UserLogin = () => {
     return (
         <div className="userIcon">
             {isMobile ? null : <GuestUser/>}
-            <ModalWebLogin/>
             <Box sx={{display: {xs: 'flex', md: 'none'}}}>
                 <IconButton
                     size="large"
@@ -186,6 +132,7 @@ const UserLogin = () => {
                 </IconButton>
             </Box>
             {renderMobileMenu}
+            <UserLoginForm open={open} handleClose={handleClose}/>
         </div>
     );
 };
