357fbe42df7552a67d4bffced0268881ef4d6cab
zwang1997 <zwang684@uottawa.ca>
2022-10-05 17:38:48 -0400

    ADD login and signup to the routes

--- view/src/App.js
+++ view/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import Header from './components/Header/Header';
+import {useContext} from "react";
 import {Route, Routes} from "react-router-dom";
+
+// pages
+import Header from './components/Header';
 import Storefront from "./pages/Storefront/Storefront";
 import Checkout from "./pages/Checkout/Checkout";
 import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 import ProductGallery from "./pages/ProductGallery/ProductGallery";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
-import {useContext} from "react";
-
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+// context
 import {FirebaseUserCollectionContext} from "./context/ContextStorage";
 
 function App() {
@@ -16,13 +20,15 @@ function App() {
     return (
         <FirebaseUserCollectionContext.Provider value={userCollectionRef}>
             <div className="App">
-                <Header/>
+                <Header />
                 <Routes>
                     <Route path="/" element={<Storefront/>}/>
                     <Route path="/product/" element={<ProductDetail/>}/>
                     <Route path="/products/" element={<ProductGallery/>}/>
                     <Route path="/cart" element={<ShoppingCart/>}/>
                     <Route path="/checkout" element={<Checkout/>}/>
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
                 </Routes>
             </div>
         </FirebaseUserCollectionContext.Provider>
