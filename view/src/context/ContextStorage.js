836db662495f713f8dc5c8629013ad14a6214a7b
zwang1997 <zwang684@uottawa.ca>
2022-10-08 00:11:33 -0400

    REFACTOR loading Context with AppContextProvider

--- view/src/context/ContextStorage.js
+++
@@ -1,5 +0,0 @@
-import {createContext} from "react";
-import {collection} from "firebase/firestore";
-import {db} from "../firebase-config";
-
-export const FirebaseUserCollectionContext = createContext(collection(db, "users"));
