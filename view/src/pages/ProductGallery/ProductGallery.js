0fabeb55684f7d7437c7d290c3460eecd38d976c
Liuyi Chen <liuyichen@Liuyis-MacBook-Air.local>
2022-10-12 18:32:49 -0400

    made a card

--- view/src/pages/ProductGallery/ProductGallery.js
+++ view/src/pages/ProductGallery/ProductGallery.js
@@ -4,14 +4,6 @@ import ProductCard from "../../components/ProductCard/ProductCard";
 const ProductGallery = () => {
     return (
         <div>
-            <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css"/>
-            <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css"/>
-            <link rel="stylesheet" href="css/elegant-icons.css" type="text/css"/>
-            <link rel="stylesheet" href="css/magnific-popup.css" type="text/css"/>
-            <link rel="stylesheet" href="css/nice-select.css" type="text/css"/>
-            <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css"/>
-            <link rel="stylesheet" href="css/slicknav.min.css" type="text/css"/>
-            <link rel="stylesheet" href="css/style.css" type="text/css"/> 
             <Typography
                 component="h2"
                 variant="h5"
