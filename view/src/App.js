b2ba6c244c1312adabbc2f77fecf66f95f05773f
LydiaJiaruiLi <JIARUILI@CMAIL.CARLETON.CA>
2022-10-13 05:37:42 -0400

    ADD shopping cart feature

--- view/src/App.js
+++ view/src/App.js
@@ -1,34 +1,44 @@
 import './App.css';
-import Header from './components/Header/Header';
 import {Route, Routes} from "react-router-dom";
+
+// pages
+import Header from './components/Header';
 import Storefront from "./pages/Storefront/Storefront";
 import Checkout from "./pages/Checkout/Checkout";
 import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
 import ProductGallery from "./pages/ProductGallery/ProductGallery";
 import ProductDetail from "./pages/ProductDetail/ProductDetail";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import CheckoutSuccess from "./pages/Checkout/CheckoutSuccess";
-import {useContext} from "react";
 
-import {FirebaseUserCollectionContext} from "./context/ContextStorage";
+// context
+import {AppContextProvider} from "./context/AppContextProvider";
+import {PayPalScriptProvider} from "@paypal/react-paypal-js";
 
 function App() {
-    const userCollectionRef = useContext(FirebaseUserCollectionContext);
 
     return (
-        <FirebaseUserCollectionContext.Provider value={userCollectionRef}>
-            <div className="App">
-                <Header/>
-                <Routes>
-                    <Route path="/" element={<Storefront/>}/>
-                    <Route path="/product/" element={<ProductDetail/>}/>
-                    <Route path="/products/" element={<ProductGallery/>}/>
-                    <Route path="/cart" element={<ShoppingCart/>}/>
-                    <Route path="/checkout" element={<Checkout/>}/>
-                    <Route path="/checkoutSuccess" element={<CheckoutSuccess/>}/>
-                </Routes>
-            </div>
-        </FirebaseUserCollectionContext.Provider>
+        <AppContextProvider>
+            <PayPalScriptProvider options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID}}>
+                <div className="App">
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Storefront/>}/>
+                        <Route path="/product/" element={<ProductDetail/>}/>
+                        <Route path="/products/" element={<ProductGallery/>}/>
+                        <Route path="/cart" element={<ShoppingCart/>}/>
+                        <Route path="/checkout" element={<Checkout/>}/>
+                        <Route path="/checkoutSuccess" element={<CheckoutSuccess/>}/>
+                        <Route path="/sign-in" element={<SignIn />} />
+                        <Route path="/sign-up" element={<SignUp />} />
+                    </Routes>
+                </div>
+            </PayPalScriptProvider>
+        </AppContextProvider>
     );
 }
 
 export default App;
+
+
