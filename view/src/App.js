30d036a1199acf9418c3b06644c28bd57cdce8c5
HaoJing-Leo <hjing098@uottawa.ca>
2022-10-15 09:30:49 -0400

    update App.js

--- view/src/App.js
+++ view/src/App.js
@@ -1,34 +1,8 @@
 import "./App.css";
-import Header from "./components/Header/Header";
-import { Route, Routes } from "react-router-dom";
-import Storefront from "./pages/Storefront/Storefront";
-import Checkout from "./pages/Checkout/Checkout";
-import ShoppingCart from "./pages/ShoppingCart/ShoppingCart";
-import ProductGallery from "./pages/ProductGallery/ProductGallery";
-import { useContext } from "react";
-
-import { FirebaseUserCollectionContext } from "./context/ContextStorage";
-import Links from "./components/External/Links";
-import PreProduct from "./pages/ProductDetail";
+import RouterProps from "./router";
 
 function App() {
-  const userCollectionRef = useContext(FirebaseUserCollectionContext);
-
-  return (
-    <FirebaseUserCollectionContext.Provider value={userCollectionRef}>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Storefront />} />
-          <Route path="/product" element={<PreProduct />} />
-          <Route path="/products/" element={<ProductGallery />} />
-          <Route path="/cart" element={<ShoppingCart />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-        <Links />
-      </div>
-    </FirebaseUserCollectionContext.Provider>
-  );
+  return <RouterProps />;
 }
 
 export default App;
