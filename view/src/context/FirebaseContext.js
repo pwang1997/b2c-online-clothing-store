efef0a6c24ef548c05c64c3b1a197ba48ea8ce00
zwang684 <puck19970418@gmail.com>
2022-10-21 14:06:46 -0400

    ADD FirebaseStorage to AppContext

--- view/src/context/FirebaseContext.js
+++ view/src/context/FirebaseContext.js
@@ -1,6 +1,6 @@
-import { createContext, useContext } from "react";
-import { collection } from "firebase/firestore";
-import { db } from "../firebase-config";
+import {createContext, useContext} from "react";
+import {collection} from "firebase/firestore";
+import {db, storage} from "../firebase-config";
 
 export const FirebaseUserCollectionContext = createContext(collection(db, "users"));
 
@@ -12,4 +12,10 @@ export const FirebaseProductCollectionContext = createContext(collection(db, "pr
 
 export function useFirebaseProductCollection() {
     return useContext(FirebaseProductCollectionContext);
+}
+
+export const FirebaseStorageContext = createContext(storage);
+
+export function useFirebaseStorage() {
+    return useContext(FirebaseStorageContext);
 }
