3e48a859da375c8174ba051b32b3449115117da0
zwang684 <puck19970418@gmail.com>
2022-11-19 17:45:00 -0500

    Update Product Gallery UI

--- view/src/pages/ProductGallery/index.js
+++ view/src/pages/ProductGallery/index.js
@@ -1,32 +1,34 @@
-import React, { useEffect, useState } from "react";
-import { Grid } from "@mui/material";
+import React, {useEffect, useState, Fragment} from "react";
+import {Grid} from "@mui/material";
 import ProductCard from "../../components/ProductCard/ProductCard";
-import { useLocation } from "react-router-dom";
+import {useLocation} from "react-router-dom";
 import {fetchProductsByCategoryService} from "../../services/ProductService";
 import {useFirebaseProductCollection} from "../../context/FirebaseContext";
 
-export default function ProductGallery () {
-  const location = useLocation();
+export default function ProductGallery() {
+    const location = useLocation();
 
-  const productContext = useFirebaseProductCollection();
-  const [products, setProducts] = useState();
+    const productContext = useFirebaseProductCollection();
+    const [products, setProducts] = useState();
 
-  useEffect(() => {
-    if(location.state?.category) {
-      console.log(location.state?.category);
-      fetchProductsByCategoryService(productContext, location.state.category, setProducts);
-    }
-  }, [location.state]);
+    useEffect(() => {
+        if (location.state?.category) {
+            console.log(location.state?.category);
+            fetchProductsByCategoryService(productContext, location.state.category, setProducts);
+        }
+    }, [location.state]);
 
-  return (
-    <Grid container gap={2} sx={{ justifyContent: "space-evenly" }}>
-      {products?.map((item) => {
-        return (
-          <Grid key={item.id}>
-            <ProductCard product={item} />
-          </Grid>
-        );
-      })}
-    </Grid>
-  );
+    return (
+        <Fragment>
+            <Grid container gap={2} sx={{justifyContent: "space-evenly"}}>
+                {products?.map((item) => {
+                    return (
+                        <Grid key={item.id}>
+                            <ProductCard product={item}/>
+                        </Grid>
+                    );
+                })}
+            </Grid>
+        </Fragment>
+    );
 };
