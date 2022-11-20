b1f9c08f9d71b7a3281b3bf5b7baf2f685a891f1
zwang684 <puck19970418@gmail.com>
2022-11-20 16:11:05 -0500

    Add redis service for fetching prodcuts and images

--- redis-server/src/firebase-config.js
+++ redis-server/src/firebase-config.js
@@ -1,23 +1,15 @@
-const firebaseApp = require("firebase/app"); 
-const firebaseStorage = require("firebase/storage");
-
+const {initializeApp, cert} = require('firebase-admin/app')
+const {getFirestore} = require('firebase-admin/firestore')
+const serviceAccount = require('./creds.json')
 const dotenv = require("dotenv")
 
-dotenv.config()
+dotenv.config();
 
-const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.measurementId
-};
+initializeApp({
+    credential: cert(serviceAccount),
+    storageBucket: process.env.BUCKET_URL
+})
 
-// Initialize Firebase
-const app = firebaseApp.initializeApp(firebaseConfig);
-const storage = firebaseStorage.getStorage(app, `gs://${process.env.storageBucket}`);
+const db = getFirestore()
 
-exports.storage = storage;
+module.exports = {db};
--- redis-server/src/routes/ProductRoutes.js
+++ redis-server/src/routes/ProductRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const ProductController = require("../controllers/ProductController");
-
+const {db} = require("../firebase-config");
 const router = express.Router();
 
 
-router.post('/api/v1/image/fetch', ProductController.fetchLeetCodeSolution);
-
-router.post('/api/v1/image/fetch', ProductController.fetchLeetCodeSolution);
-
+router.get('/fetch/find-all', ProductController.fetchAllProducts);
+router.get('/fetch/', ProductController.fetchProductByCondition);
+router.get('/image/get/', ProductController.fetchProductImage);
+router.post('/image/set/', ProductController.setProductImageUrl);
 
 module.exports = router;
